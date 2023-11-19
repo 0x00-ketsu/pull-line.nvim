40992227d9598b2859ea09c6ee393de8408ec0d3
0x00-ketsu <ketsu.chin.121@gmail.com>
2023-11-19 17:19:43 +0800

    feat: add lua format config file & reformat

--- dev/tests.lua
+++ dev/tests.lua
@@ -1,10 +1,10 @@
-local enums = require('pull-line.enums')
 local Line = require('pull-line.line')
+local enums = require('pull-line.enums')
 
 local M = {}
 
 M.execute = function()
-  require('pull-line').setup {}
+  require('pull-line').setup({})
 
   local line = Line:init()
   line:copy(enums.DIRECTIONS.ABOVE)
